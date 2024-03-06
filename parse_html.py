eb5a6e3cdaec856cfabd9703fbd5be9c7e12f3a3
Johanna <hl105@wellesley.edu>
2024-03-05 20:51:51 -0500

    add more features

--- parse_html.py
+++ parse_html.py
@@ -17,14 +17,20 @@ def extract_vid_info(video_file):
             return element.text
         else:
             return None
+    def safe_dict_find(data, key):
+        try:
+            val = data[key]
+            return val
+        except:
+            return None
 
     with open(video_file, 'r') as f:
         print(video_file)
         vid_num = video_file.split(".")[0].split("_")[-1]
-
+        data = None
         contents = f.read()
         soup = BS(contents, "html.parser")
-        url, username, nickname, description, music, like_count, share_count, comment_count, play_count, collect_count, comments = None,None,None,None,None,None,None,None,None,None,None
+        url, username, nickname, description, location_created, suggested_words, video_duration, is_ad, music, like_count, share_count, comment_count, play_count, collect_count, comments = None,None,None,None,None,None,None,None,None,None,None,None,None,None,None
         try:
             url = soup.find("meta", property="og:url")['content']
         except:
@@ -37,14 +43,20 @@ def extract_vid_info(video_file):
         try:
             script_tag = soup.find('script', text=re.compile('stats'))
             script_content = script_tag.string 
-            data = json.loads(script_content)["__DEFAULT_SCOPE__"]['webapp.video-detail']['itemInfo']['itemStruct']["stats"] #json to python dict, and keep looking
-            like_count = data['diggCount']
-            share_count = data['shareCount']
-            comment_count = data['commentCount']
-            play_count = data['playCount']
-            collect_count = data['collectCount']
+            data = json.loads(script_content)["__DEFAULT_SCOPE__"]['webapp.video-detail']['itemInfo']['itemStruct'] #json to python dict, and keep looking
         except:
             pass
+        if data:
+            location_created = safe_dict_find(data,'locationCreated')
+            suggested_words = safe_dict_find(data,'suggestedWords')
+            video_duration = safe_dict_find(data['video'],'duration')
+            is_ad = safe_dict_find(data,'isAd')
+            like_count = safe_dict_find(data["stats"],'diggCount')
+            share_count = safe_dict_find(data["stats"],'shareCount')
+            comment_count = safe_dict_find(data["stats"],'commentCount')
+            play_count = safe_dict_find(data["stats"],'playCount')
+            collect_count = safe_dict_find(data["stats"],'collectCount')
+        
         #print("\nlike:",like_count, "\nshare:",share_count, "\ncomment:", comment_count, "\nplay:", play_count, "\ncollect:", collect_count)
 
         comment_div = soup.find_all("p", {"class": "css-xm2h10-PCommentText e1g2efjf6"})
@@ -53,7 +65,7 @@ def extract_vid_info(video_file):
             comments.append(comment.text)
         f.close()
 
-    return vid_num, url, username, nickname, description, music, like_count, share_count, comment_count, play_count, collect_count, comments
+    return vid_num, url, username, nickname, description, location_created, suggested_words, video_duration, is_ad, music, like_count, share_count, comment_count, play_count, collect_count, comments
     
 
 
@@ -62,14 +74,18 @@ def all_videos_info(video_files,data_folder):
     param: list of video_files saved by TikTokScraper.py
     return: dataframe with all video_info
     """
-    infos = {'vid_num':[], 'url':[],'username':[],'nickname':[],'description':[], 'music':[], 'like_count':[], 'share_count':[], 'comment_count':[], 'play_count':[], 'collect_count':[], 'comments':[]}
+    infos = {'vid_num':[], 'url':[],'username':[],'nickname':[],'description':[], 'location_created':[],'suggested_words':[],'video_duration':[],'is_add':[],'music':[], 'like_count':[], 'share_count':[], 'comment_count':[], 'play_count':[], 'collect_count':[], 'comments':[]}
     for vid in video_files:
-        vid_num, url, username, nickname, description, music, like_count, share_count, comment_count, play_count, collect_count, comments = extract_vid_info(vid)
+        vid_num, url, username, nickname, description, location_created, suggested_words, video_duration, is_ad, music, like_count, share_count, comment_count, play_count, collect_count, comments = extract_vid_info(vid)
         infos['vid_num'].append(vid_num)
         infos['url'].append(url)
         infos['username'].append(username)
         infos['nickname'].append(nickname)
         infos['description'].append(description)
+        infos['location_created'].append(location_created)
+        infos['suggested_words'].append(suggested_words)
+        infos['video_duration'].append(video_duration)
+        infos['is_add'].append(is_ad)
         infos['music'].append(music)
         infos['like_count'].append(like_count)
         infos['share_count'].append(share_count)
